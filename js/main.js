d8eae9b17c6277a7b74314d31e5a3d65ffe0c279
alexvas84 <64201758+alexvas84@users.noreply.github.com>
2020-05-26 23:48:40 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,48 @@
+$(function () {
+
+	$(".rate-star").rateYo({
+		rating: 5,
+		starWidth: "12px",
+		readOnly: true
+	});
+
+	$('.product-slider-inner').slick({
+		dots: true,
+		arrows: false,
+		slidesToShow: 4,
+		slidesToScroll: 4
+	});
+
+
+	$(".js-range-slider").ionRangeSlider({
+		type: "double",
+		min: 0,
+		max: 1000,
+		from: 0,
+		to: 600,
+		prefix: "$"
+	});
+
+	$('.icon-th-list').on('click', function () {
+		$('.product__item').addClass('list');
+		$('.icon-th-list').addClass('active');
+		$('.icon-th-large').removeClass('active');
+	});
+	$('.icon-th-large').on('click', function () {
+		$('.product__item').removeClass('list');
+		$('.icon-th-large').addClass('active');
+		$('.icon-th-list').removeClass('active');
+	});
+
+	$('.header__btn-menu').on('click', function () {
+		$('.header__box').toggleClass('active');
+	});
+
+	$('.menu__btn').on('click', function () {
+		$('.menu__list').slideToggle();
+	});
+
+
+	var mixer = mixitup('.products__inner-box');
+
+});
